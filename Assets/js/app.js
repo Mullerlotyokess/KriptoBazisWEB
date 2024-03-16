0a23e582d5559f0789b4c81c5a40ef3dd8e00f4e
Mullerlotyokess <bucifej09@gmail.com>
2024-03-16 20:43:44 +0100

    Navbar törölve, sidebarral helyezve

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,13 +1,13 @@
-var app = angular.module('KriptoBazis', ['ngRoute']);
+var app = angular.module('KriptoBazisAPP', ['ngRoute']);
 
 app.run(($rootScope) => {
     $rootScope.loggedIn = false;
-    $rootScope.appTitle = "KriptoBazis";
+    $rootScope.appTitle = "Kripto Bazis";
 
     $rootScope.emailRegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
     $rootScope.passwdRegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
 
-    $rootScope.serverUrl = 'http://localhost:8000';
+    $rootScope.serverUrl = 'http://localhost:5000';
     $rootScope.appUrl = 'http://127.0.0.1:5500/index.html';
 
     if (sessionStorage.getItem('access_token')) {
@@ -21,14 +21,6 @@ app.config(($routeProvider) => {
     $routeProvider
         .when('/main', {
             templateUrl: 'Views/main.html'
-        })
-        .when('/login', {
-            templateUrl: 'Views/login.html',
-            controller: 'authCtrl',
-        })
-        .when('/register', {
-            templateUrl: 'Views/register.html',
-            controller: 'authCtrl'
         })
         .otherwise({
             redirectTo: '/main'
