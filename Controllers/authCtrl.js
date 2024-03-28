048cc974bb5c536fa3fe2ba7df473c3ddaf3f0ca
Mullerlotyokess <bucifej09@gmail.com>
2024-03-28 12:04:46 +0100

    Bejelentkezés & Regisztráció

--- Controllers/authCtrl.js
+++ Controllers/authCtrl.js
@@ -1,12 +1,104 @@
-app.controller("authCtrl", function($scope, $rootScope) {
+app.controller("authCtrl", function($scope, $rootScope, $location) {
+    $scope.user = {};
 
+    const toastTrigger = document.getElementById('liveToastBtn')
+    const toastLiveExample = document.getElementById('liveToast')
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+
+    const { passwordRegExp, serverUrl } = $rootScope;
+    let toastcontent = document.getElementById('toastcontent');
+    
+
+    $scope.regisztracio = function(){
+
+        const {username, email, pass, passconfirm} = $scope.user;
+
+        if (username == null || email == null || pass == null || passconfirm == null){
+            toastcontent.innerText = "Nem adtál meg minden adatot!"
+            toastBootstrap.show()
+            return;
+        }
+
+        if (!email.includes('@')) {
+            toastcontent.innerText = "Nem jó E-mail formátum!"
+            toastBootstrap.show()
+            return; 
+        }
+          
+        if (pass != passconfirm){
+            toastcontent.innerText = "A jelszavak nem egyeznek!"
+            toastBootstrap.show()
+            return;
+        }
+
+        if (!pass.match(passwordRegExp)){
+            toastcontent.innerText = "Nem elég erős jelszó!"
+            toastBootstrap.show()
+            return;    
+        }
+
+        axios.get(serverUrl+'?table=users&field=email&value='+email).then(res =>{
+            if(res.data.length > 0){
+                toastcontent.innerText = "Ez az Email-cím már regisztrálva van!"
+                toastBootstrap.show()
+                return;
+            }
+            let request = {
+                table: 'users', 
+                data: { 
+                    username, 
+                    email, 
+                    pass: CryptoJS.SHA1(pass).toString()
+                }
+            }
+            axios.post(serverUrl, request).then(res =>{
+                toastcontent.innerText = "Sikeres regisztráció!"
+                toastBootstrap.show()
+                $scope.user = {};
+            });
+        });
+
+    }
+
+
+    $scope.bejelentkezes= function() {
+
+        toastcontent.innerText = "";
+
+        const { email, pass } = $scope.user;
+
+        if (email == null || pass == null){
+            toastcontent.innerText = "Nem adtál meg minden adatot!"
+            toastBootstrap.show()
+            return;
+        }
+        
+        if (!email.includes('@')) {
+          toastcontent.innerText = "Nem jó E-mail formátum!"
+          toastBootstrap.show()
+          return; 
+        }
+
+        let request = {
+            logincheck: true,
+            table: 'users',
+            email, 
+            pass: CryptoJS.SHA1(pass).toString()
+        }
+
+        axios.post(serverUrl, request).then(res => {
+            if(res.data == false){
+                toastcontent.innerText = "Rossz E-mail/Jelszó!"
+                toastBootstrap.show()
+                return;
+            }
+        });
+    }
     $scope.passReset = function()
     {
-        const toastTrigger = document.getElementById('liveToastBtn')
-        const toastLiveExample = document.getElementById('liveToast')
-        
-        const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
-        toastBootstrap.show()
+    
+      toastcontent.innerText = "Jelszó emlékeztető Email elküldve!"
+      toastBootstrap.show()
         
     
     }
