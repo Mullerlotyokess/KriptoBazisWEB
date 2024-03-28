ffd8c0844187210da1cdae18d02428c5380a1781
Mullerlotyokess <bucifej09@gmail.com>
2024-03-28 16:56:43 +0100

    Árfolyam nézet& Coinmarketcap API hívások

--- API/server.js
+++ API/server.js
@@ -28,11 +28,35 @@ app.get("/", (req, res) => {
     res.send("Backend API get /");
 });
 
-app.get("https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=X-CMC_PRO_API_KEY':'"+env.APIKEY, (req, res) => {
-    console.log(res.data)
-});
-
-
+app.get('/:crypto', (req, res) => {
+    const crypto = req.params.crypto
+    // URL BRINGS BACK ALL INFO OF THE TOKEN 
+    const url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?symbol=${crypto}`
+    // URL2 BRINGS BACK THE CURRENT QUOTE AND PRICE DATA
+    const url2 = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=${crypto}`
+  
+    request.get({
+      url: url2,
+      json: true,
+      headers: {
+        'X-CMC_PRO_API_KEY': '7902d9b3-8a32-4a60-ab3d-ec68cae957e8'
+      }
+    }, (error, response, data) => {
+  
+      if (error) {
+        return res.send({
+          error: error
+        });
+      }
+  
+      res.send({
+        data: data
+      });
+  
+    });
+  
+  });
+  
 app.listen(port, () => {
     console.log(`Server running on: ${port} port`);
 });
--- Controllers/currenciesCtrl.js
+++ Controllers/currenciesCtrl.js
@@ -1,4 +1,6 @@
 app.controller("currenciesCtrl", function($scope, $rootScope, $location) {
-
+    setTimeout(() => {
+        axios.get(``)
+    }, 500);
 
 });
