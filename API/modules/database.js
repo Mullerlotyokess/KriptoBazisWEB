a77e42abaec94ab0650d5afe3efd1fe268ac02bf
Mullerlotyokess <bucifej09@gmail.com>
2024-04-10 09:14:39 +0200

    API crypto hívás működik

--- API/modules/database.js
+++ API/modules/database.js
@@ -5,7 +5,8 @@ var jwt = require('jsonwebtoken');
 require("dotenv").config();
 var mysql = require("mysql");
 const { response } = require('express');
-
+const axios  = require('axios');
+const request = require('request')
 
 var pool  = mysql.createPool({
   connectionLimit : 10,
@@ -22,19 +23,19 @@ var pool  = mysql.createPool({
 router.get('/', function (req, res) {
     res.send('Simpe NodeJS Backend API');
 });
-  
-/*router.get('/:crypto', (req, res) => {
+
+router.get('/:crypto', (req, res) => {
     const crypto = req.params.crypto
     // URL BRINGS BACK ALL INFO OF THE TOKEN 
     const url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?symbol=${crypto}`
     // URL2 BRINGS BACK THE CURRENT QUOTE AND PRICE DATA
     const url2 = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=${crypto}`
   
-    req.get({
+    request.get({
       url: url2,
       json: true,
       headers: {
-        'X-CMC_PRO_API_KEY': '7902d9b3-8a32-4a60-ab3d-ec68cae957e8'
+        'X-CMC_PRO_API_KEY': process.env.APIKEY
       }
     }, (error, response, data) => {
   
@@ -50,7 +51,8 @@ router.get('/', function (req, res) {
   
     });
   
-});*/
+});
+
 
 router.post("/users", (req,res) => {
 
